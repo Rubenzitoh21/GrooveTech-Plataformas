9185b53a00fa5ccb785f22f85d6411dd9fd44967
Nelson <2220858@my.ipleiria.pt>
2024-12-20 10:57:49 +0000

    functional test example on backen

--- backend/tests/functional/LoginCest.php
+++ backend/tests/functional/LoginCest.php
@@ -4,6 +4,7 @@ namespace backend\tests\functional;
 
 use backend\tests\FunctionalTester;
 use common\fixtures\UserFixture;
+use yii\helpers\Url;
 
 /**
  * Class LoginCest
@@ -30,15 +31,40 @@ class LoginCest
     /**
      * @param FunctionalTester $I
      */
-    public function loginUser(FunctionalTester $I)
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnPage(Url::toRoute('/site/login'));
+    }
+
+    public function loginVazio(FunctionalTester $I)
     {
         $I->amOnRoute('/site/login');
-        $I->fillField('Username', 'erau');
-        $I->fillField('Password', 'password_0');
-        $I->click('login-button');
+        $I->amGoingTo('Login sem credenciais');
+        $I->fillField('LoginForm[username]', '');
+        $I->fillField('LoginForm[password]', '');
+        $I->click('Login');
+        $I->see('Este campo não pode ficar em branco.');
+    }
 
-        $I->see('Logout (erau)', 'form button[type=submit]');
-        $I->dontSeeLink('Login');
-        $I->dontSeeLink('Signup');
+    public function credenciaisVazias(FunctionalTester $I)
+    {
+        $I->amOnRoute('site/login');
+        $I->amGoingTo('Login com credenciais erradas');
+        $I->fillField('LoginForm[username]', 'adminn');
+        $I->fillField('LoginForm[password]', 'admin321');
+        $I->click('Login');
+        $I->see('Nome de utilizador ou palavra-passe incorretos.');
+    }
+
+    public function validarLoginAdmin(FunctionalTester $I)
+    {
+        $I->fillField('LoginForm[username]', 'admin');
+        $I->seeInField('LoginForm[username]', 'admin');
+        $I->fillField('LoginForm[password]', 'admin1234');
+        $I->seeInField('LoginForm[password]', 'admin1234');
+        $I->click('#login-form button[type=submit]');
+
+        $I->see('Início');
+        $I->dontSeeLink('#login-form button[type=submit]');
     }
 }
